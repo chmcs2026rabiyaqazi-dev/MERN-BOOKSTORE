1d782c867b550dc9b84476a9111f39112f7ade0a
chmcs2026rabiyaqazi-dev <chmcs2026.rabiya.qazi@gmail.com>
2026-02-25 08:44:15 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app=express();
 const port=process.env.PORT || 3001
 app.use(cors({ 
-    origin:"http://localhost:5173"
+   // origin:"http://localhost:5173"
 }))
 app.use(express.json())
 app.use("/books", bookRoutes)
