ac3ad4d2512d16a8eaf6ce88f380c066b671e3f7
chmcs2026rabiyaqazi-dev <chmcs2026.rabiya.qazi@gmail.com>
2026-02-25 09:06:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from "react-router"
 import HomePage from "./pages/HomePage"
 import CreatePage from './pages/CreatePage'
-import BookDetailPage from './pages/BookDetailpage'
+import BookDetailPage from './pages/BookDetailPage'
 
 const App = () => {
   return (
